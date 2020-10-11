ce6f369f72b79ce8f34e74c9c1b9966144d80db5
sau22rane <scrane22301@gmail.com>
2020-10-11 17:40:55 +0530

    Genome folder added

---
+++ Local/Genome/CMakeFiles/Genome.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Genome.dir/Genome.cpp.o"
+  "Genome"
+  "Genome.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Genome.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
